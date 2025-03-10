b7411f837ac86ae6ba5bdb1f92762e357c16e9a5
virtualrwanda <virtualrwandatechnology@gmail.com>
2025-03-10 23:46:46 +0200

    Update fiber.py
    False debuging

--- fiber.py
+++ fiber.py
@@ -758,4 +758,4 @@ def health_check():
 
 if __name__ == '__main__':
     # Use a single worker to reduce memory usage
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), threaded=True)
